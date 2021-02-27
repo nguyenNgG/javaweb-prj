25f9b4b1f968553f3a9c41fce56fe67ff61dc890
nguyenNgN <ngianguyen81@gmail.com>
2021-02-27 07:57:33 +0700

    fix dao closing

--- MVC2SE1503/src/java/nguyenng/registration/RegistrationDAO.java
+++ MVC2SE1503/src/java/nguyenng/registration/RegistrationDAO.java
@@ -102,16 +102,15 @@ public class RegistrationDAO implements Serializable {
                 } //end while traversing result
             } //end if con is opened
         } finally {
-            if (con != null) {
-                con.close();
-            }
             if (rs != null) {
                 rs.close();
             }
             if (stm != null) {
                 stm.close();
             }
-
+            if (con != null) {
+                con.close();
+            }
         }
 
     }
